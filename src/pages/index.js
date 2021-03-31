57bc8beb0af666b7423a0c2646e87034bfea199c
youngjin <teen14y@gmail.com>
2021-03-31 12:28:22 +0900

    set absolute imports and path to src as "@"

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,7 @@
 import Head from 'next/head';
+
+import Button from '@/components';
+
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
@@ -12,7 +15,7 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Button />
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
