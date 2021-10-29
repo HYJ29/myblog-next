97201851e7367140249efe591ff4ec9cdd8894ae
youngjin <teen14y@gmail.com>
2021-10-30 01:00:21 +0900

    add lottie on about page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
 import Image from 'next/image';
 import Fade from 'react-reveal/Fade';
+import Lottie from 'react-lottie';
+
+import byyoungjinLineAnimation from 'assets/lottie/byyoungjin-line-animation.json';
 
 import { DefaultLayout } from '@/components/layout';
 import styles from './style.module.scss';
@@ -13,36 +16,41 @@ export default function AboutPage() {
   // }, []);
   return (
     <DefaultLayout>
-      <section className={styles.sectionContainer_1}>
-        {/* <Fade>
-          <div className={styles.profileImageContainer}>
-            <Image
-              src={'/images/grandCanyon.jpeg'}
-              layout="fill"
-              objectFit="cover"
-              className={styles.profileImage}
-              placeholder="blur"
+      <div className={styles.container}>
+        <section className={styles.sectionContainer_0}>
+          <div className={styles.lottieContainer}>
+            <Lottie
+              options={{
+                loop: false,
+                autoplay: true,
+                animationData: byyoungjinLineAnimation,
+                rendererSettings: {
+                  preserveAspectRatio: 'xMidYMid slice',
+                },
+              }}
+              isStoped={false}
+              isFaused={false}
             />
           </div>
-        </Fade> */}
-        <Fade top>
-          <h3>I love being service developer.</h3>
-          <h3>Who can deploy awesome ideas to the world.</h3>
-          <h3>I love to join project with mind of owner.</h3>
-          <h3>I love riding bicycle around 'Han' river.</h3>
-          <h3>I love running in the morning with music on my aftershock.</h3>
-        </Fade>
-      </section>
-      <section className={styles.sectionContainer_2}>
-        <Fade top>
-          <ul className={styles.postListContainer}>about</ul>
-        </Fade>
-      </section>
-      <section className={styles.sectionContainer_3}>
-        <Fade top>
-          <ul className={styles.postListContainer}>about</ul>
-        </Fade>
-      </section>
+        </section>
+        <section className={styles.sectionContainer_1}>
+          <Fade top>
+            <h3>I love being service developer,</h3>
+            <h3>who can deploy awesome ideas to the world.</h3>
+          </Fade>
+        </section>
+        <section className={styles.sectionContainer_2}>
+          <Fade top>
+            <h3>I love to join challenging projects.</h3>
+          </Fade>
+        </section>
+        <section className={styles.sectionContainer_3}>
+          <Fade top>
+            <h3>I love riding bicycle around 'Han' river.</h3>
+            <h3>I love running in the morning with music on my aftershock.</h3>
+          </Fade>
+        </section>
+      </div>
     </DefaultLayout>
   );
 }
